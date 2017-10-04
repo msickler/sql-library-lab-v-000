6989ea7e4588928ad46391da75a9886bb73a4d05
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 13:53:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(species) FROM characters
-  GROUP BY species SORT BY COUNT(species) DESC LIMIT 1; "
+  GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1; "
+  
 end
 
 def select_name_and_series_subgenres_of_authors
