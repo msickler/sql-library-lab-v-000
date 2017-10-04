9adbca315283893d66eacaeea38cab27271616e5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 13:41:57 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,39 +1,15 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (
-  (3, "magic", 3, 6),
-  (10, "love", 9, 4)
-  );
+ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES (
-  (3, "mama", 1993, 2),
-  (4, "papa", 1992, 2),
-  (10, "juliet", 1888, 4),
-  (11, "romeo", 1756, 1),
-  (8, "anything", 1989, 3),
-  (7, "more", 1978, 5)
-);
-
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (
-  (5, "hermione", "shut up woman", "witch", 2, 1),
-  (6, "yuki", "leave me alone", "alien", 3, 2),
-  (7, "draco", "love me", "wizard", 4, 5),
-  (8, "harry", "i'm a wizard", "muggle", 10, 11),
-  (9, "shinji", "why me", "loser", 6, 2),
-  (10, "asuka", "won't say i'm in love", "redhead", 6, 9),
-  (11, "rei", "muted for life", "clone", 4, 4),
-  (12, "sansa", "i'm not dumb", "princess", 6, 11),
-);
-
-INSERT INTO subgenres (id, name) VALUES (
-  (8, "vampires"),
-  (9, "suicide")
-);
-
-INSERT INTO authors (id, name) VALUES (
-  (2, "Tolkien"),
-  (3, "Murakami")
-);
-
-INSERT INTO character_books (id, book_id, character_id) VALUES (
-  (1, 2, 3), (2, 3, 3), (3, 4, 2), (4, 5, 1), (5, 6, 4), (6, 5, 5), (7, 7, 2), (8, 10, 9),
-  (9, 2, 6), (10, 10, 9), (11, 12, 13), (12, 7, 8), (13, 8, 7), (14, 13, 12), (15, 12, 10), (16, 10, 1)
-);
+ INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
+ 
+ INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
+ 
+ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+ 
+ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+ 
+ INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
+ 
+ INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+ 
+ INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
