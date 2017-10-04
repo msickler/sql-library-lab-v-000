2f2dfc28e64058bee067b1b0df048451fe16356e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 13:36:53 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,12 +29,12 @@ INSERT INTO sub_genres (id, name) VALUES (
   (9, "suicide")
 );
 
-INSERT INTO authord (id, name) VALUES (
+INSERT INTO authors (id, name) VALUES (
   (2, "Tolkien"),
   (3, "Murakami")
 );
 
-INSERT INTO character_books (id, character_id, book_id) VALUES (
+INSERT INTO character_books (id, book_id, character_id) VALUES (
   (1, 2, 3), (2, 3, 3), (3, 4, 2), (4, 5, 1), (5, 6, 4), (6, 5, 5), (7, 7, 2), (8, 10, 9),
   (9, 2, 6), (10, 10, 9), (11, 12, 13), (12, 7, 8), (13, 8, 7), (14, 13, 12), (15, 12, 10), (16, 10, 1)
 );
