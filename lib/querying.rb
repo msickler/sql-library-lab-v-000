36ff6a2e2dd8e98bf761849e049397eef3091c58
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 14:01:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  JOIN characters
+  ON characters.series_id = series.id
+  GROUP BY characters.species ORDER BY species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
